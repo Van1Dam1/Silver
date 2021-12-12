f4d2f7e76ba6f84497d6ea3709a5e3876c2effa5
Van1Dam1 <96021488+Van1Dam1@users.noreply.github.com>
2021-12-12 23:03:57 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/* 
+Дмитриев Иван Александрович
+Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ)
+ по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+ */
+
+double m;
+double h;
+double I;
+Console.Write ("Введите ваш вес в кг: ");
+string c = Console.ReadLine ();
+m = Convert.ToDouble(c);
+Console.Write ("Введите ваш рост в метрах: ");
+string b = Console.ReadLine ();
+h = Convert.ToDouble(b);
+I = m / (h*h);
+Console.WriteLine ("Ваш индекс массы тела: " + I);
